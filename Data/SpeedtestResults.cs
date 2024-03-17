d70eddb733be83b0734b5ce1236ad5c4a8d48f45
Jack <L00158710@atu.ie>
2024-03-17 00:25:12 +0000

    d

--- Data/SpeedtestResults.cs
+++ Data/SpeedtestResults.cs
@@ -1,10 +1,12 @@
-﻿namespace BlazorVersa.Data
+﻿using System;
+
+namespace BlazorVersa.Data
 {
     public class SpeedtestResults
     {
         public double UploadSpeed { get; set; }
         public double DownloadSpeed { get; set; }
-        public int PingSpeed { get; set; }
+        public double PingSpeed { get; set; }
         public DateTime Timestamp { get; set; }
     }
 }
