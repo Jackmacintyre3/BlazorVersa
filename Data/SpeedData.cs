5bf0f29c9a4ae56386ab0cd10af301c27ccfea40
Jack <L00158710@atu.ie>
2024-03-21 09:01:26 +0000

    working

---
+++ Data/SpeedData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorVersa.Data
+{
+    internal class SpeedData
+    {
+    }
+}
